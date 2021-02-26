6f720b57eed46b76a9bc460ccadc18d409ca16b8
oscarleonnogales <oscarleonnogales@gmail.com>
2021-02-25 22:54:06 -0800

    Styling

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -2,17 +2,29 @@ const toggleDropdownBtn = document.querySelector('[data-toggle-dropdown-btn]');
 const dropdown = document.querySelector('[data-dropdown]');
 const overlay = document.querySelector('[data-overlay]');
 overlay.addEventListener('click', () => {
-	overlay.classList.remove('active');
-	if (dropdown) dropdown.classList.remove('active');
+	deActivate([overlay]);
+	if (dropdown) {
+		deActivate([dropdown, toggleDropdownBtn]);
+	}
 });
 if (toggleDropdownBtn) {
 	toggleDropdownBtn.addEventListener('click', () => {
 		if (dropdown.classList.contains('active')) {
-			overlay.classList.remove('active');
-			dropdown.classList.remove('active');
+			deActivate([overlay, dropdown, toggleDropdownBtn]);
 		} else {
-			overlay.classList.add('active');
-			dropdown.classList.add('active');
+			activate([overlay, dropdown, toggleDropdownBtn]);
 		}
+	});
+}
+
+function activate(elements) {
+	elements.forEach((element) => {
+		element.classList.add('active');
+	});
+}
+
+function deActivate(elements) {
+	elements.forEach((element) => {
+		element.classList.remove('active');
 	});
 }
